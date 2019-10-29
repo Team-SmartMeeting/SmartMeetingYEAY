e39279b786f11218446468d5d24c6e621081caaa
ak5ina <43174060+ak5ina@users.noreply.github.com>
2019-10-29 17:13:13 +0100

    Efter registrering er man logget ind
    Efter registrering er man logget ind på sin main side

--- app/src/main/java/com/example/smartmeeting/Register.java
+++ app/src/main/java/com/example/smartmeeting/Register.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -60,7 +61,18 @@ public class Register extends AppCompatActivity {
                                 progressBar.setVisibility(View.GONE);
 
                                 if (task.isSuccessful()) {
-                                    finish();
+                                    firebaseAuth.signInWithEmailAndPassword(txtEmail.getText().toString(), txtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            finish();
+                                            Intent in = new Intent(getApplicationContext(), MeetingOverview.class);
+                                            startActivity(in);
+                                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+                                        }
+                                    });
+
+
 
                                 } else {
                                     Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
