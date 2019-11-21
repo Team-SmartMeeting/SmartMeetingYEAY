383cf2bab626c064804fbfc2136a368464c4228d
Andreas <mail.Tailspot@gmail.com>
2019-11-21 16:18:37 +0100

    Startet på ViewProfile og EditProfile klasser

--- app/src/main/java/com/example/smartmeeting/EditProfile.java
+++ app/src/main/java/com/example/smartmeeting/EditProfile.java
@@ -2,13 +2,53 @@ package com.example.smartmeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class EditProfile extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+
+        TextView textName = (TextView) findViewById(R.id.name);
+        TextView textPhone = (TextView) findViewById(R.id.phonenumber);
+        TextView textEmail = (TextView) findViewById(R.id.email);
+        TextView textCompany = (TextView) findViewById(R.id.company);
+        TextView textAddress = (TextView) findViewById(R.id.address);
+        TextView textZipCode = (TextView) findViewById(R.id.zip_code);
+        TextView textCountry = (TextView) findViewById(R.id.country);
+
+        /*
+        textName.setHint();
+        textPhone.setHint();
+        textEmail.setHint();
+        textCompany.setHint();
+        textAddress.setHint();
+        textZipCode.setHint();
+        textCountry.setHint();
+        */
+
+        Button editProfile = (Button) findViewById(R.id.btn_save_profile);
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), ViewProfile.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/smartmeeting/ViewProfile.java
+++ app/src/main/java/com/example/smartmeeting/ViewProfile.java
@@ -2,13 +2,52 @@ package com.example.smartmeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ViewProfile extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+        TextView textName = findViewById(R.id.name);
+        TextView textPhone = findViewById(R.id.phonenumber);
+        TextView textEmail = findViewById(R.id.email);
+        TextView textCompany = findViewById(R.id.company);
+        TextView textAddress = findViewById(R.id.address);
+        TextView textZipCode = findViewById(R.id.zip_code);
+        TextView textCountry = findViewById(R.id.country);
+        /*
+        textName.setText();
+        textPhone.setText();
+        textEmail.setText();
+        textCompany.setText();
+        textAddress.setText();
+        textZipCode.setText();
+        textCountry.setText();
+        */
+        Button editProfile = findViewById(R.id.btn_edit_profile);
+            editProfile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent startIntent = new Intent(getApplicationContext(), EditProfile.class);
+                    startActivity(startIntent);
+                }
+            });
+
+
     }
 }
