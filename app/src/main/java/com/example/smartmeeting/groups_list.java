b6e68b5f6ac1fdbbf0097f07569eb33bb95360cf
icodenaked90 <simonphilipsen1990@gmail.com>
2019-11-24 19:58:03 +0100

    groups

--- app/src/main/java/com/example/smartmeeting/groups_list.java
+++ app/src/main/java/com/example/smartmeeting/groups_list.java
@@ -2,7 +2,10 @@ package com.example.smartmeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class groups_list extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class groups_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups_list);
+
+        Button btn = findViewById(R.id.btn_new_group);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), add_group.class);
+                startActivity(intent);
+            }
+        });
     }
 }
