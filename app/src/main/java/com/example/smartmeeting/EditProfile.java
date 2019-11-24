c75486051d8a31e1e22621d88b8f503e17f2bfc1
AndreasOeS <mail.andreas.s@gmail.com>
2019-11-24 20:54:07 +0100

    Tilføget tool til profile

--- app/src/main/java/com/example/smartmeeting/EditProfile.java
+++ app/src/main/java/com/example/smartmeeting/EditProfile.java
@@ -50,5 +50,47 @@ public class EditProfile extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
+
+
+        //Menuen
+        Button btn_profile = findViewById(R.id.btn_profile_menu);
+        Button btn_meetings = findViewById(R.id.btn_meeting_menu);
+        Button btn_groupe = findViewById(R.id.btn_groupes_menu);
+        Button btn_contacts = findViewById(R.id.btn_contacts_menu);
+
+        btn_contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ContactList.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn_groupe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), groups_list.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn_meetings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeetingOverview.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
