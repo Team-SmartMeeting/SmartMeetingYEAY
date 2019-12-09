29e2f9b0fb4c12f1cd1ea23972efb6a1a521a80b
icodenaked90 <simonphilipsen1990@gmail.com>
2019-12-09 17:30:04 +0100

    agenda fixed

--- app/src/main/java/com/example/smartmeeting/Activitys/Agenda.java
+++ app/src/main/java/com/example/smartmeeting/Activitys/Agenda.java
@@ -1,4 +1,4 @@
-package com.example.smartmeeting;
+package com.example.smartmeeting.Activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.smartmeeting.R;
 
 public class Agenda extends AppCompatActivity {
 
