5cc79ce19e5c5fa6286639cc3fe810c7fbea8c7e
icodenaked90 <simonphilipsen1990@gmail.com>
2019-12-09 17:06:12 +0100

    Agenda

--- app/src/main/java/com/example/smartmeeting/Agenda.java
+++ app/src/main/java/com/example/smartmeeting/Agenda.java
@@ -2,7 +2,10 @@ package com.example.smartmeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Agenda extends AppCompatActivity {
 
@@ -10,5 +13,64 @@ public class Agenda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+        Button btn_paticipants = findViewById(R.id.btn_add_participant);
+        Button btn_topics = findViewById(R.id.btn_add_topic);
+
+//        btn_topics.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent_topic = new Intent(getApplicationContext(),addtopic.class);
+//                startActivity(intent_topic);
+//            }
+//        });
+
+//        btn_paticipants.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent_participants = new Intent(getApplicationContext(), participants.class);
+//                startActivity(intent_participants);
+//            }
+//        });
+
+        //Menuen
+        Button btn_profile = findViewById(R.id.btn_profile_menu);
+        Button btn_meetings = findViewById(R.id.btn_meeting_menu);
+        Button btn_groupe = findViewById(R.id.btn_groupes_menu);
+        Button btn_contacts = findViewById(R.id.btn_contacts_menu);
+
+        btn_contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ContactList.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn_groupe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), groups_list.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn_meetings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeetingOverview.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
+
+
