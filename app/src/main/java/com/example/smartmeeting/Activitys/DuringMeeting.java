b009e030335c4f44b27d6bd29cfc79c67fb53990
icodenaked90 <simonphilipsen1990@gmail.com>
2020-01-08 16:33:21 +0100

    small change

---
+++ app/src/main/java/com/example/smartmeeting/Activitys/DuringMeeting.java
@@ -0,0 +1,16 @@
+package com.example.smartmeeting.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartmeeting.R;
+
+public class DuringMeeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_during_meeting);
+    }
+}
