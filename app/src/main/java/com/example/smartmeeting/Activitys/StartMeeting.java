534b422d558831de4d2bb390d879c0b07858f835
AndreasOeS <43959623+AndreasOeS@users.noreply.github.com>
2020-01-14 12:27:33 +0100

    Lavet agenda list til Startmeeting

--- app/src/main/java/com/example/smartmeeting/Activitys/StartMeeting.java
+++ app/src/main/java/com/example/smartmeeting/Activitys/StartMeeting.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.smartmeeting.R;
+
+import java.util.ArrayList;
 
 public class StartMeeting extends AppCompatActivity {
 
@@ -15,6 +19,22 @@ public class StartMeeting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_meeting);
+
+
+        ListView topicList = findViewById(R.id.listview_topics);
+
+        ArrayList<String> listItems=new ArrayList<String>();
+        listItems.add("Topic 1");
+        listItems.add("Topic 2");
+        listItems.add("Topic 3");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, listItems);
+
+        topicList.setAdapter(arrayAdapter);
+
+        arrayAdapter.notifyDataSetChanged();
+
 
 
 
@@ -29,4 +49,6 @@ public class StartMeeting extends AppCompatActivity {
 
 
     }
+
+
 }
